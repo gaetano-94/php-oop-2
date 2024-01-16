c367a6bb0794c937aa584613e02821337d2e6a84
gaetano-94 <gaetano.panico1994@gmail.com>
2024-01-16 16:28:44 +0100

    feat: classe per prodotti

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php
+
+//Classe per il prodotto 
+class Prodotto
+{
+
+  public $titolo;
+  public $descrizione;
+  public $prezzo;
+  //construttore per i prodotti 
+  public function __construct($titolo, $descrizione, $prezzo,)
+  {
+
+    $this->titolo = $titolo;
+    $this->descrizione = $descrizione;
+    $this->prezzo = $prezzo;
+  }
+}
